b69014a230aeb34af991e8b768e37c591e198d2f
S1D1T1 <156350598+S1D1T1@users.noreply.github.com>
2024-06-19 00:54:32 -0400

    Update format.js

--- format.js
+++ format.js
@@ -1,6 +1,7 @@
 /*
 {
   "name":          String(required)
+  "comment":       String(optional)
   "sample-image":  URL(optional)
   "resource-urls": Array(optional)
   "settings":      object(required)
